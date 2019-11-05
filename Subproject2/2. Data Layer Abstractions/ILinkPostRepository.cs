4d92c83a151b5e2a89a4f06122a3a5782f7bac8a
Ivan Spajic <ivan.spajic@gmail.com>
2019-11-05 17:39:05 +0100

    Added repository abstractions.

---
+++ Subproject2/2. Data Layer Abstractions/ILinkPostRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2._Data_Layer_Abstractions
+{
+    public interface ILinkPostRepository
+    {
+    }
+}
---
+++ Subproject2/2. Data Layer Abstractions/ISubmissionRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2._Data_Layer_Abstractions
+{
+    public interface ISubmissionRepository
+    {
+
+    }
+}
