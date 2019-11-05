2bee90c068da8968759ae2e4772c0037af3b3da5
Manish Shrestha <manish.05shrestha@gmail.com>
2019-11-05 19:04:23 +0100

    Added skeleton for Submissions controllers

---
+++ Subproject2/3. Data Layer/SubmissionRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace _3._Data_Layer
+{
+    class SubmissionRepository
+    {
+        private readonly DbContext databaseContext;
+
+        public SubmissionRepository(DbContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+    }
+}
---
+++ Subproject2/SOVA/Controllers/CommentsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _2._Data_Layer_Abstractions;
+
+namespace _1._SOVA.Controllers
+{
+    public class CommentsController
+    {
+        private readonly ICommentRepository commentRepository;
+
+        public CommentsController(ICommentRepository commentRepository)
+        {
+            this.commentRepository = commentRepository;
+        }
+    }
+}
