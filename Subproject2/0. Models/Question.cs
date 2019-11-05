6ff121129f9a08c615edc19f01a8d6950c80e1c1
ozgey99 <42248791+ozgey99@users.noreply.github.com>
2019-11-05 10:30:49 +0100

    added model files

---
+++ Subproject2/0. Models/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0._Models
+{
+    public class Question : Submission
+    {
+        public string Title { get; set; }
+        public DateTime ClosedDate { get; set; }
+    }
+}
---
+++ Subproject2/0. Models/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0._Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string TagString { get; set; }
+    }
+}
