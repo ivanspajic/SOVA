b842c471c4796755f0850bc80aa9c228b1f5c38d
shus0001 <32295145+shus0001@users.noreply.github.com>
2019-11-06 22:33:31 +0100

    Added new class in data layer called answersrepository

---
+++ Subproject2/3. Data Layer/AnswerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _0._Models;
+using _3._Data_Layer.Database_Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+
+namespace _3._Data_Layer
+{
+    public class AnswerRepository
+    {
+        private readonly SOVAContext databaseContext;
+
+        public AnswerRepository(SOVAContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+
+        }
+
+        public IEnumerable<Answer> GetLatestTenAnswers()
+        {
+            return databaseContext.Answers.OrderByDescending(s => s.Submission.CreationDate).Take(10);
+        }
+
+    }
+}
