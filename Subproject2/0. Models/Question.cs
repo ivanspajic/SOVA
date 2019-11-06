12a997d134f0474389fe1ea21ef110c492ec7f6d
Manish Shrestha <manish.05shrestha@gmail.com>
2019-11-06 21:38:28 +0100

    Reverted property

--- Subproject2/0. Models/Question.cs
+++ Subproject2/0. Models/Question.cs
@@ -6,7 +6,6 @@ namespace _0._Models
 {
     public class Question
     {
-        public int Id { get; set; }
         public int SubmissionId { get; set; }
         public string Title { get; set; }
         public DateTime? ClosedDate { get; set; }
