4de09bfe745359855d4baa976711bc4d4e0bec75
Ivan Spajic <ivan.spajic@gmail.com>
2019-11-07 08:58:29 +0100

    Added repository interface implementations and mapped the controllers.

--- Subproject2/1. SOVA/Startup.cs
+++ Subproject2/1. SOVA/Startup.cs
@@ -19,6 +19,7 @@ namespace SOVA
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
             services.AddTransient<IHistoryRepository>(provider => new HistoryRepository(new SOVAContext()));
         }
 
@@ -38,6 +39,7 @@ namespace SOVA
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
+                endpoints.MapControllers();
             });
         }
     }
--- Subproject2/3. Data Layer/QuestionRepository.cs
+++ Subproject2/3. Data Layer/QuestionRepository.cs
@@ -5,10 +5,11 @@ using _0._Models;
 using _3._Data_Layer.Database_Context;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using _2._Data_Layer_Abstractions;
 
 namespace _3._Data_Layer
 {
-    public class QuestionRepository
+    public class QuestionRepository : IQuestionRepository
     {
         private readonly SOVAContext databaseContext;
 
