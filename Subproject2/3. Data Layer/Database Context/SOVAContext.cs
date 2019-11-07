efe94e0aad6acbf63cb4fe0253cb2b27b0cba4a2
ozgey99 <42248791+ozgey99@users.noreply.github.com>
2019-11-07 11:45:17 +0100

    Update SOVAContext.cs

--- Subproject2/3. Data Layer/Database Context/SOVAContext.cs
+++ Subproject2/3. Data Layer/Database Context/SOVAContext.cs
@@ -1,6 +1,5 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Text;
 using _0._Models;
 using Microsoft.EntityFrameworkCore;
@@ -44,9 +43,8 @@ namespace _3._Data_Layer.Database_Context
 
                 foreach (var property in entityType.GetProperties())
                 {
-                    if (property.Name == "SubmissionId")
-                        Debug.Write("yo");
                     var propertyName = ModifyString(property.Name);
+
                     if (property.Name.Contains("String"))
                     {
                         propertyName = property.Name.Substring(0, property.Name.Length - "String".Length).ToLower();
@@ -59,10 +57,8 @@ namespace _3._Data_Layer.Database_Context
     }
     public class SOVAContext : DbContext
     {
-        private const string ConnectionString = "host=localhost;db=stackoverflow;uid=postgres;pwd=is131095";
+        private const string ConnectionString = "host=localhost;db=stackoverflow;uid=postgres;pwd=";
         public DbSet<Annotation> Annotations { get; set; }
-
-        public DbSet<Submission> Submissions { get; set; }
         public DbSet<Answer> Answers { get; set; }
         //public DbSet<Comment> Comments { get; set; }
         public DbSet<History> History { get; set; }
@@ -71,6 +67,7 @@ namespace _3._Data_Layer.Database_Context
         public DbSet<Question> Questions { get; set; }
         public DbSet<QuestionsTag> QuestionsTags { get; set; }
         public DbSet<SoMember> SoMembers { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
         public DbSet<Tag> Tags { get; set; }
         public DbSet<UserHistory> UserHistory { get; set; }
         public DbSet<User> Users { get; set; }
@@ -84,8 +81,10 @@ namespace _3._Data_Layer.Database_Context
         {
             modelBuilder.CreateMap();
             modelBuilder.Entity<Annotation>().HasKey(a => new { a.SubmissionId, a.UserId });
-            modelBuilder.Entity<Question>().HasKey(a => new { a.SubmissionId } );
-            modelBuilder.Entity<Answer>().HasKey(a => new { a.SubmissionId });
+            modelBuilder.Entity<Question>().HasKey(q => q.SubmissionId);
+            modelBuilder.Entity<Answer>().HasKey(a => a.SubmissionId);
+            modelBuilder.Entity<Question>().HasOne(q => q.Submission).WithMany().HasForeignKey(q => q.SubmissionId).IsRequired(true);
+            modelBuilder.Entity<Answer>().HasOne(a => a.Submission).WithMany().HasForeignKey(a => a.SubmissionId).IsRequired(true);
             modelBuilder.Entity<Marking>().HasKey(m => new { m.UserId, m.SubmissionId });
             modelBuilder.Entity<QuestionsTag>().HasKey(q => new { q.QuestionId, q.TagId });
             modelBuilder.Entity<UserHistory>().HasKey(uh => new { uh.UserId, uh.HistoryId });
