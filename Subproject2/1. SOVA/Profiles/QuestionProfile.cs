340614c1131387e5a13242824ccda0220fdd2927
Ozge <ozgey99@gmail.com>
2019-11-08 14:58:56 +0100

    added all profile classes

---
+++ Subproject2/1. SOVA/Profiles/QuestionProfile.cs
@@ -0,0 +1,18 @@
+﻿using _0._Models;
+using _1._SOVA.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1._SOVA.Profiles
+{
+    public class QuestionProfile : Profile
+    {
+        public QuestionProfile()
+        {
+            CreateMap<Question, QuestionDto>();
+        }
+    }
+}
