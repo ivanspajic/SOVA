c0b094bc03f6665be1b9a27ff2b4e851c13075dd
shus0001 <32295145+shus0001@users.noreply.github.com>
2019-11-09 23:41:13 +0100

    AuthService class in Middleware folder of Sova service is updated

--- Subproject2/1. SOVA/Middleware/AuthService.cs
+++ Subproject2/1. SOVA/Middleware/AuthService.cs
@@ -1,11 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using _3._Data_Layer.Database_Context ;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using _2._Data_Layer_Abstractions;
 
-namespace _1._SOVA.Middleware
+namespace WebServiceSimple.Middleware
 {
     public class AuthService
     {
+        private readonly RequestDelegate _next;
+
+        public AuthService(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IDataService dataService)
+        {
+            Program.CurrentUser = null;
+            var auth = context.Request.Headers["Authorization"];
+            if (auth != StringValues.Empty)
+            {
+                Program.CurrentUser = dataService.GetUser(auth);
+            }
+
+            await _next(context);
+        }
     }
 }
