b4877f7c022e49e2cb6a877545a5ac2f933d7b4b
Ozge <ozgey99@gmail.com>
2019-11-10 13:53:34 +0100

    fixed minor things

--- Subproject2/3. Data Layer/QuestionRepository.cs
+++ Subproject2/3. Data Layer/QuestionRepository.cs
@@ -31,11 +31,9 @@ namespace _3._Data_Layer
             return _databaseContext.Questions.Find(submissionId);
         }
 
-        // TODO: Fix this function call. Currently it fails with error: `column "chocholate" does not exist`
-
         public IEnumerable<SearchResult> SearchQuestions(string queryString)
         {
-            return _databaseContext.SearchResults.FromSqlRaw("SELECT * from best_match({0})", queryString);
+            return _databaseContext.SearchResults.FromSqlRaw("SELECT * from best_match_weighted({0})", queryString);
         }
 
     }
