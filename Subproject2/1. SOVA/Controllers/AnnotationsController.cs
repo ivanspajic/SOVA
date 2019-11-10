a5f80474549f2cb35869fb6e8e43bdc6151bfcca
Ozge <ozgey99@gmail.com>
2019-11-10 11:32:53 +0100

    added methods to annotation controller and a method to annotation repository

--- Subproject2/1. SOVA/Controllers/AnnotationsController.cs
+++ Subproject2/1. SOVA/Controllers/AnnotationsController.cs
@@ -22,5 +22,66 @@ namespace _1._SOVA.Controllers
             _annotationRepository = annotationRepository;
             _mapper = mapper;
         }
+
+        [HttpGet("{submissionId}", Name = nameof(GetAnnotation))]
+        public ActionResult GetAnnotation(int submissionId)
+        {
+            var ant = _annotationRepository.GetBySubmissionId(submissionId);
+            if (ant == null)
+            {
+                return NotFound();
+            }
+            return Ok(CreateAnnotationDto(ant));
+        }
+
+        [HttpPost("{submissionId}")]
+        public ActionResult CreateAnnotation(AnnotationForCreation antDto, int submissionId)
+        {
+            var ant = _mapper.Map<Annotation>(antDto);
+            ant.SubmissionId = submissionId;
+            _annotationRepository.Create(ant.AnnotationString, submissionId);
+            return CreatedAtRoute(
+                nameof(GetAnnotation),
+                new { submissionId = ant.SubmissionId },
+                CreateAnnotationDto(ant));
+        }
+
+        [HttpPut("{submissionId}")]
+        public ActionResult UpdateAnnotation(
+            int submissionId, Annotation annotation)
+        {
+            if (_annotationRepository.GetBySubmissionId(submissionId) == null)
+            {
+                return NotFound();
+            }
+            annotation.SubmissionId = submissionId;
+            _annotationRepository.Update(annotation.AnnotationString, annotation.SubmissionId);
+            return Ok();
+        }
+
+        [HttpDelete("{submissionId}")]
+        public ActionResult DeleteAnnotation(int submissionId)
+        {
+            if (_annotationRepository.Delete(submissionId))
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        ///////////////////
+        //
+        // Helpers
+        //
+        ///////////////////
+
+        private AnnotationDto CreateAnnotationDto(Annotation annotation)
+        {
+            var dto = _mapper.Map<AnnotationDto>(annotation);
+            dto.Link = Url.Link(
+                    nameof(GetAnnotation),
+                    new { annotationId = annotation.SubmissionId });
+            return dto;
+        }
     }
 }
