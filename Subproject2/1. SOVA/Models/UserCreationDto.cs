e0fc368070e132f3be969afe6f271a364ee93c95
Manish Shrestha <manish.05shrestha@gmail.com>
2019-11-10 17:57:54 +0100

    Auth startup

---
+++ Subproject2/1. SOVA/Models/UserCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1._SOVA.Models
+{
+    public class UserCreationDto
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Subproject2/1. SOVA/PasswordService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace _1._SOVA
+{
+    public class PasswordService
+    {
+        private static RNGCryptoServiceProvider _rng = new RNGCryptoServiceProvider();
+
+        public static string GenerateSalt(int size)
+        {
+            var buffer = new byte[size];
+            _rng.GetBytes(buffer);
+            return Convert.ToBase64String(buffer);
+        }
+
+        public static string HashPassword(string pwd, string salt, int size)
+        {
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                pwd,
+                Encoding.UTF8.GetBytes(salt),
+                KeyDerivationPrf.HMACSHA256,
+                10000,
+                size));
+        }
+    }
+}
