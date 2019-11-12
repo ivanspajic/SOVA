e80bbacfa65cb1992c30105d086d60b915bf746a
shus0001 <32295145+shus0001@users.noreply.github.com>
2019-11-12 11:55:12 +0100

    history controller updated

--- Subproject2/2. Data Layer Abstractions/IHistoryRepository.cs
+++ Subproject2/2. Data Layer Abstractions/IHistoryRepository.cs
@@ -7,6 +7,6 @@ namespace _2._Data_Layer_Abstractions
 {
     public interface IHistoryRepository
     {
-        History GetSingleForUser(int id, string username);
+        History GetHistoryForUser(int id);
     }
 }
