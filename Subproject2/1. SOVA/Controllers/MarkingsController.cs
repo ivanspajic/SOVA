f6c6cae83e6db3211b6107ee38e2cd6cd98afb4e
Ozge <ozgey99@gmail.com>
2019-11-12 11:25:08 +0100

    added bookmarking feature

---
+++ Subproject2/1. SOVA/Controllers/MarkingsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _0._Models;
+using _1._SOVA.Models;
+using _2._Data_Layer_Abstractions;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _1._SOVA.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class MarkingsController : ControllerBase
+    {
+        private readonly IMarkingRepository _markingRepository;
+        private IMapper _mapper;
+
+        public MarkingsController(IMarkingRepository markingRepository, IMapper mapper)
+        {
+            _markingRepository = markingRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{userId}/markedposts", Name = nameof(GetMarkedPostsForUser))]
+        public ActionResult GetMarkedPostsForUser([FromQuery] PagingAttributes pagingAttributes, int userId)
+        {
+            var posts = _markingRepository.GetMarkedPosts(userId, pagingAttributes);
+            return Ok(CreateResult(posts, userId, pagingAttributes));
+        }
+
+        [HttpPut("{submissionId}/{userId}")]
+        public ActionResult UpdateBookmark(int submissionId, int userId)
+        {
+            if (_markingRepository.IsMarked(submissionId, userId))
+            {
+                _markingRepository.RemoveBookmark(submissionId, userId);
+                return Ok();
+            }
+
+            _markingRepository.Bookmark(submissionId, userId);
+            return Ok();
+        }
+
+        private object CreateResult(IEnumerable<Submission> posts, int userId, PagingAttributes attr)
+        {
+            var totalItems = _markingRepository.NoOfMarkings(userId);
+            var numberOfPages = Math.Ceiling((double)totalItems / attr.PageSize);
+
+            var prev = attr.Page > 0
+                ? CreatePagingLink(attr.Page - 1, attr.PageSize)
+                : null;
+            var next = attr.Page < numberOfPages - 1
+                ? CreatePagingLink(attr.Page + 1, attr.PageSize)
+                : null;
+
+            return new
+            {
+                totalItems,
+                numberOfPages,
+                prev,
+                next,
+                items = posts.Select(CreateSubmissionDto)
+            };
+        }
+
+        private SubmissionDto CreateSubmissionDto(Submission sub)
+        {
+            var dto = _mapper.Map<SubmissionDto>(sub);
+            dto.Link = Url.Link(
+                    nameof(GetMarkedPostsForUser),
+                    new { submissionId = sub.Id });
+            return dto;
+        }
+
+        private string CreatePagingLink(int page, int pageSize)
+        {
+            return Url.Link(nameof(GetMarkedPostsForUser), new { page, pageSize });
+        }
+    }
+}
--- Subproject2/2. Data Layer Abstractions/IMarkingRepository.cs
+++ Subproject2/2. Data Layer Abstractions/IMarkingRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _0._Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,10 @@ namespace _2._Data_Layer_Abstractions
 {
     public interface IMarkingRepository
     {
-
+        bool Bookmark(int submissionId, int userId);
+        int NoOfMarkings(int userId);
+        IEnumerable<Submission> GetMarkedPosts(int userId, PagingAttributes pagingAttributes);
+        bool IsMarked(int submissionId, int userId);
+        bool RemoveBookmark(int submissionId, int userId);
     }
 }
