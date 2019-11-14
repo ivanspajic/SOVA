bf30d9ac5a8f172b8e8a133618a385155a328a09
Ozge <ozgey99@gmail.com>
2019-11-14 12:15:50 +0100

    fixed build error

--- Subproject2/2. Data Layer Abstractions/IQuestionRepository.cs
+++ Subproject2/2. Data Layer Abstractions/IQuestionRepository.cs
@@ -14,6 +14,6 @@ namespace _2._Data_Layer_Abstractions
         Question GetById(int submissionId);
         IEnumerable<SearchResult> SearchQuestions(string queryString, PagingAttributes pagingAttributes);
         IEnumerable<Question> GetLinkedPosts(int questionId);
-        IEnumerable<Tag> GetTags(int questionId);
+        IEnumerable<Question> GetTags(int questionId);
     }
 }
