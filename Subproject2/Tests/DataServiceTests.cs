8e670b7b68df75c254fd80627dd070a1412f806c
Manish Shrestha <manish.05shrestha@gmail.com>
2019-11-14 18:28:05 +0100

    Commented out history test

--- Subproject2/Tests/DataServiceTests.cs
+++ Subproject2/Tests/DataServiceTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using _0._Models;
 using _3._Data_Layer;
 using _3._Data_Layer.Database_Context;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Xunit;
 
 namespace Tests
@@ -32,7 +33,7 @@ namespace Tests
             _annotationRepository = new AnnotationRepository(new SOVAContext(_connectionString));
             _answerRepository = new AnswerRepository(new SOVAContext(_connectionString));
             _commentRepository = new CommentRepository(new SOVAContext(_connectionString));
-            _historyRepository = new HistoryRepository(new SOVAContext(_connectionString));
+            //_historyRepository = new HistoryRepository(new SOVAContext(_connectionString));
             _linkPostRepository = new LinkPostRepository(new SOVAContext(_connectionString));
             _markingRepository = new MarkingRepository(new SOVAContext(_connectionString));
             _questionRepository = new QuestionRepository(new SOVAContext(_connectionString));
@@ -290,7 +291,7 @@ namespace Tests
             IEnumerable<Answer> answers = _answerRepository.GetAnswersForQuestionById(questionId, testAttributes);
 
             // Assert
-            Assert.All(answers, (answer) => 
+            Assert.All(answers, (answer) =>
             {
                 Assert.NotNull(answer.Submission);
                 Assert.All(answer.Comments, (comment) => Assert.NotNull(comment.CommentSubmission));
@@ -339,7 +340,7 @@ namespace Tests
             };
 
             SOVAContext databaseContext = new SOVAContext(_connectionString);
-            
+
             IEnumerable<Comment> expectedComments = databaseContext.Comments
                                                         .Where(comment => comment.SubmissionId == submissionId)
                                                         .Skip(pageNumber * pageSize)
@@ -390,30 +391,30 @@ namespace Tests
             Assert.All(comments, (comment) => Assert.NotNull(comment.CommentSubmission));
         }
 
-        [Fact]
-        public void GetHistoryById_ValidArgument()
-        {
-            // Arrange
-            int historyId = 1;
+        //[Fact]
+        //public void GetHistoryById_ValidArgument()
+        //{
+        //    // Arrange
+        //    int historyId = 1;
 
-            // Act
-            History history = _historyRepository.GetHistoryById(historyId);
+        //    // Act
+        //    History history = _historyRepository.GetUserHistoryByUserId(historyId);
 
-            // Assert
-            Assert.Equal(historyId, history.Id);
-        }
+        //    // Assert
+        //    Assert.Equal(historyId, history.Id);
+        //}
 
-        [Theory]
-        [InlineData(0)]
-        [InlineData(-1)]
-        public void GetHistoryById_InvalidArgument(int historyId)
-        {
-            // Act
-            History history = _historyRepository.GetHistoryById(historyId);
+        //[Theory]
+        //[InlineData(0)]
+        //[InlineData(-1)]
+        //public void GetHistoryById_InvalidArgument(int historyId)
+        //{
+        //    // Act
+        //    History history = _historyRepository.GetHistoryById(historyId);
 
-            // Assert
-            Assert.Equal(default, history);
-        }
+        //    // Assert
+        //    Assert.Equal(default, history);
+        //}
 
         [Fact]
         public void GetLinkPostByQuestionAndLinkedPostIds_ValidArgument()
