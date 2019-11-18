c47f09c5ecd65e1f94cd8acb8b9bf91073a918d4
Ozge <ozgey99@gmail.com>
2019-11-18 19:46:02 +0100

    removed redundant properties

--- Subproject2/SOVA/Models/UserHistoryDto.cs
+++ Subproject2/SOVA/Models/UserHistoryDto.cs
@@ -8,8 +8,6 @@ namespace SOVA.Models
         public string Link { get; set; }
         public int Id { get; set; }
         public int UserId { get; set; }
-        public string SearchTerm { get; set; }
-        public DateTime SearchDate { get; set; }
         public History UserHistory { get; set; }
     }
 }
