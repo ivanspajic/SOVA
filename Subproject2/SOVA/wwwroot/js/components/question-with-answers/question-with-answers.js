2bacbf4a6e384dcf1435502db45b2a6d66046924
Manish Shrestha <manish.05shrestha@gmail.com>
2019-12-01 21:07:03 +0100

    updated to async/await

--- Subproject2/SOVA/wwwroot/js/components/question-with-answers/question-with-answers.js
+++ Subproject2/SOVA/wwwroot/js/components/question-with-answers/question-with-answers.js
@@ -1,2 +1,15 @@
-﻿define([], function () {
+﻿define(["knockout", "dataService"], function (ko, ds) {
+    return function () {
+        var questionIdFromParams = window.location.split("/")[1];
+        console.log(questionIdFromParams);
+        var questionWithAnswer = ko.observable();
+        ds.getQuestionByIdWithAnswers((questionIdFromParams, data) => {
+            questionWithAnswer(questionIdFromParams, data);
+        });
+
+        return {
+            questionWithAnswer
+        };
+
+    };
 });
---
+++ Subproject2/SOVA/wwwroot/js/viewModel.js
@@ -0,0 +1,8 @@
+﻿define([], function () {
+    return function (params) {
+        var name = params.name;
+        return {
+            name
+        };
+    };
+});
