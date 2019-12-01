588c7b8ba994821c42de90d6a1236b183f99276c
Manish Shrestha <manish.05shrestha@gmail.com>
2019-12-01 20:31:09 +0100

    clean up

--- Subproject2/SOVA/wwwroot/js/app.js
+++ Subproject2/SOVA/wwwroot/js/app.js
@@ -1,15 +1,2 @@
 ﻿define([], function () {
-    //var currentComponent = ko.observable("landing-page");
-    //var currentParams = ko.observable({});
-    //var goToQuestionDetails = (questionId) => {
-    //    currentParams({ name: `${questionId}` });
-    //    currentComponent("question-with-answers");
-    //};
-
-    //return {
-    //    currentParams,
-    //    currentComponent,
-    //    goToQuestionDetails
-
-    //};
 });
--- Subproject2/SOVA/wwwroot/js/components/landing-page/landing-page.js
+++ Subproject2/SOVA/wwwroot/js/components/landing-page/landing-page.js
@@ -5,19 +5,8 @@
             questions(data.items.$values);
         });
 
-        var currentComponent = ko.observable("landing-page");
-        var currentParams = ko.observable({});
-        var goToQuestionDetails = (questionId) => {
-            currentParams({ name: `${questionId}` });
-            currentComponent("question-with-answers");
-        };
-
         return {
-            currentParams,
-            currentComponent,
-            goToQuestionDetails,
             questions
-
         };
 
     };
