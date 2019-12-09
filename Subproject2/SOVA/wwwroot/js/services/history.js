ce4af36cb61c164ac3b3c0722089475ca34baa18
Ivan Spajic <ivan.spajic@gmail.com>
2019-12-09 21:03:13 +0100

    Added another component state to the store.

--- Subproject2/SOVA/wwwroot/js/services/history.js
+++ Subproject2/SOVA/wwwroot/js/services/history.js
@@ -1,18 +1,38 @@
 ﻿define(["store"], function (store) {
+    const questionWithAnswersComponent = "question-with-answers";
+    const loginPageComponent = "login-page";
+    const signupPageComponent = "signup-page";
     store.subscribe(function () {
         var state = store.getState();
         var stringifiedState = JSON.stringify(state);
+        console.log(window.history);
         window.history.pushState(stringifiedState, null);
     });
     window.onpopstate = function (event) {
+        console.log(event.state);
         if (event.state === null) {
-            console.log("landing-page");
+            var firstLandingPageAction = store.actions.landingPage;
+            store.dispatch(firstLandingPageAction);
         } else {
             var parsedState = JSON.parse(event.state);
-            console.log(parsedState.activeComponent);
-            Object.keys(store.actions).forEach(key => {
-                console.log(store.actions[key]);
-            });
+            var action = getMatchingAction(parsedState.activeComponent);
+            if (typeof storeAction === 'function') {
+                action = storeAction(parsedState.selectedQuestionId);
+            }
+            console.log(action);
+            store.dispatch(action);
+        }
+    }
+    var getMatchingAction = function (component) {
+        switch (component) {
+            case questionWithAnswersComponent:
+                return store.actions.selectQuestion;
+            case loginPageComponent:
+                return store.actions.selectMenu;
+            case signupPageComponent:
+                return store.actions.signupUser;
+            default:
+                return store.actions.landingPage;
         }
     }
 });
--- Subproject2/SOVA/wwwroot/js/services/store.js
+++ Subproject2/SOVA/wwwroot/js/services/store.js
@@ -2,6 +2,7 @@
     const selectQuestion = "SELECT_QUESTION";
     const selectMenu = "SELECT_MENU";
     const signupUser = "SIGN_UP";
+    const landingPage = "LANDING_PAGE";
     var subscribers = [];
     var currentState = {};
     var getState = () => currentState;
@@ -22,6 +23,8 @@
                 return Object.assign({}, state, { selectedMenu: action.selectedMenu, activeComponent: "login-page" });
             case signupUser:
                 return Object.assign({}, state, { activeComponent: "signup-page" });
+            case landingPage:
+                return Object.assign({}, state, { activeComponent: "landing-page" });
             default:
                 return state;
         }
@@ -52,6 +55,10 @@
                 type: signupUser,
                 activeComponent: "signup-page"
             }
+        },
+        landingPage: {
+            type: landingPage,
+            activeComponent: "landing-page"
         }
     };
 
