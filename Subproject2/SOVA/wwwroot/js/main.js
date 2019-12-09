f9627ae56f1ee6d2ef3af5e5bec8e213482af2f1
Ivan Spajic <ivan.spajic@gmail.com>
2019-12-09 14:32:42 +0100

    Work in progress.

--- Subproject2/SOVA/wwwroot/js/main.js
+++ Subproject2/SOVA/wwwroot/js/main.js
@@ -5,8 +5,8 @@
         knockout: "../lib/knockout/build/output/knockout-latest",
         text: "../lib/requirejs-text/text",
         dataService: "services/dataservice",
-        store: "services/store"
-        //history: "services/history"
+        store: "services/store",
+        history: "services/history"
     },
     shim: {
         jqcloud: ["jquery"],
@@ -40,7 +40,7 @@ require(["knockout"], function (ko) {
     });
 });
 
-require(["knockout", "app", "store"], function (ko, app, store) {
+require(["knockout", "app", "store", "history"], function (ko, app, store, history) {
     store.subscribe(() => console.log(store.getState()));
     ko.applyBindings(app);
 });
--- Subproject2/SOVA/wwwroot/js/services/history.js
+++ Subproject2/SOVA/wwwroot/js/services/history.js
@@ -1,6 +1,18 @@
 ﻿define(["store"], function (store) {
     store.subscribe(function () {
         var state = store.getState();
-        window.history.pushState(state, "");
+        var stringifiedState = JSON.stringify(state);
+        window.history.pushState(stringifiedState, null);
     });
+    window.onpopstate = function (event) {
+        if (event.state === null) {
+            console.log("landing-page");
+        } else {
+            var parsedState = JSON.parse(event.state);
+            console.log(parsedState.activeComponent);
+            Object.keys(store.actions).forEach(key => {
+                console.log(store.actions[key]);
+            });
+        }
+    }
 });
