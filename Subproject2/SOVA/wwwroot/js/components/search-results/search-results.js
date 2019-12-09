477c523b5e22f932f5442273afe553e26b35969a
Ozge <ozgey99@gmail.com>
2019-12-09 21:05:54 +0100

    fixes

--- Subproject2/SOVA/wwwroot/js/components/search-results/search-results.js
+++ Subproject2/SOVA/wwwroot/js/components/search-results/search-results.js
@@ -2,16 +2,12 @@
     var activeComponent = ko.observable("search-results");
     var results = ko.observableArray([]);
 
-    ds.getResults((data) => {
-        results(data.items.$values);
-    });
-
     var search = function () {
         var queryTerm = document.getElementById("searchterm").value;
         ds.search(queryTerm, (data) => {
             results(data);
-        }
-    });
+        });
+    };
 
     var selectResult = (data, searchResult) => {
         store.dispatch(store.actions.selectResult(searchResult.Id));
@@ -21,8 +17,7 @@
         return {
             activeComponent,
             results,
-            selectResult,
-            queryTerm
+            selectResult
         };
     };
 });
