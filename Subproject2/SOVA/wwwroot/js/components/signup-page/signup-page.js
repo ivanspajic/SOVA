81daf80d373013064a081b315ef8b0040b34f086
Manish Shrestha <manish.05shrestha@gmail.com>
2019-12-09 22:16:38 +0100

    return on signup page

--- Subproject2/SOVA/wwwroot/js/components/signup-page/signup-page.js
+++ Subproject2/SOVA/wwwroot/js/components/signup-page/signup-page.js
@@ -1,12 +1,17 @@
 ﻿define(['knockout', 'dataService', 'store'], function (ko, ds, store) {
+    return function () {
 
-    var activeComponent = ko.observable("signup-page");
+        var activeComponent = ko.observable("signup-page");
 
-    var createUser = function () {
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        ds.createUser(username, password);
-        store.dispatch(store.actions.selectMenu());
+        var createUser = function () {
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
+            ds.createUser(username, password);
+            store.dispatch(store.actions.selectMenu());
+        }
+        return {
+            activeComponent,
+            createUser
+        }
     }
-
 });
