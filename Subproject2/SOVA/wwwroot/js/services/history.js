0e42098c6e4aed02700aafcde73c4444b6886715
Ivan Spajic <ivan.spajic@gmail.com>
2019-12-10 14:52:43 +0100

    History works.

--- Subproject2/SOVA/wwwroot/js/services/history.js
+++ Subproject2/SOVA/wwwroot/js/services/history.js
@@ -2,37 +2,41 @@
     const questionWithAnswersComponent = "question-with-answers";
     const loginPageComponent = "login-page";
     const signupPageComponent = "signup-page";
+    const searchPageComponent = "search-results";
+    var popState = false;
     store.subscribe(function () {
         var state = store.getState();
         var stringifiedState = JSON.stringify(state);
-        console.log(window.history);
-        window.history.pushState(stringifiedState, null);
+        if (!popState) {
+            window.history.pushState(stringifiedState, null);
+        } else {
+            popState = false;
+        }
     });
     window.onpopstate = function (event) {
         console.log(event.state);
+        popState = true;
         if (event.state === null) {
-            var firstLandingPageAction = store.actions.landingPage;
+            var firstLandingPageAction = store.actions.landingPage();
             store.dispatch(firstLandingPageAction);
         } else {
             var parsedState = JSON.parse(event.state);
-            var action = getMatchingAction(parsedState.activeComponent);
-            if (typeof storeAction === 'function') {
-                action = storeAction(parsedState.selectedQuestionId);
-            }
-            console.log(action);
+            var action = getMatchingAction(parsedState);
             store.dispatch(action);
         }
     }
-    var getMatchingAction = function (component) {
-        switch (component) {
+    var getMatchingAction = function (state) {
+        switch (state.activeComponent) {
             case questionWithAnswersComponent:
-                return store.actions.selectQuestion;
+                return store.actions.selectQuestion(state.selectedQuestionId);
             case loginPageComponent:
-                return store.actions.selectMenu;
+                return store.actions.selectMenu(state.selectedMenu);
             case signupPageComponent:
-                return store.actions.signupUser;
+                return store.actions.signupUser();
+            case searchPageComponent:
+                return store.actions.searching(state.searchTerm);
             default:
-                return store.actions.landingPage;
+                return store.actions.landingPage();
         }
     }
 });
