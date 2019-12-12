f35ddfb3be4cc2a2e3d8182306e17e30569e0cad
Ozge <ozgey99@gmail.com>
2019-12-12 09:47:57 +0100

    updated frontend

--- Subproject2/SOVA/wwwroot/js/components/search-results/search-results.js
+++ Subproject2/SOVA/wwwroot/js/components/search-results/search-results.js
@@ -1,24 +1,38 @@
-﻿define(['knockout', 'dataService', 'store'], function (ko, ds, store) {
-    var results = ko.observable();
+﻿define(['knockout', 'dataService', 'store', 'jqcloud'], function (ko, ds, store) {
+    return function (params) {
+        var results = ko.observable();
 
-    store.subscribe(() => {
+        var width = params.width || 200;
+        var height = params.height || 200;
+        var words = [];
+
+        store.subscribe(() => {
+            ds.search((data) => {
+                results(data.items.$values);
+            }),
+            ds.getWord2Words((data) => {
+                words = data;
+                $('#cloud').jQCloud(words,
+                    {
+                        width: width,
+                        height: height
+                    });
+            });
+        });
+
         ds.search((data) => {
             results(data.items.$values);
         });
-    });
 
-    ds.search((data) => {
-        results(data.items.$values);
-    });
+        var selectPost = (data, id, isQuestion) => {
+            store.dispatch(store.actions.selectPost(id, isQuestion.isQuestion));
+        }
 
-    var selectPost = (data, id, isQuestion) => {
-        store.dispatch(store.actions.selectPost(id, isQuestion.isQuestion));
-    }
-
-    return function () {
-        return {
-            selectPost,
-            results
+        return function () {
+            return {
+                selectPost,
+                results
+            };
         };
     };
 });
