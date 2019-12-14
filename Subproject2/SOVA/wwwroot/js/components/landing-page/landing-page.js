2c8831f871b39f12952d82b84060eb35b84dbc72
Manish Shrestha <manish.05shrestha@gmail.com>
2019-12-14 22:36:34 +0100

    responsive search and teg filter

--- Subproject2/SOVA/wwwroot/js/components/landing-page/landing-page.js
+++ Subproject2/SOVA/wwwroot/js/components/landing-page/landing-page.js
@@ -2,9 +2,30 @@
     var questions = ko.observableArray([]);
     var sessionStorage = ko.observable(localStorage.getItem("username"));
 
+    var prevPage = ko.observable();
+    var nextPage = ko.observable();
+
     ds.getQuestions((data) => {
         questions(data.items.$values);
+        nextPage(data.next);
+        prevPage(data.prev);
     });
+
+    var getNext = function () {
+        ds.getOtherPages(nextPage, (data) => {
+            questions(data.items.$values);
+            nextPage(data.next);
+            prevPage(data.prev);
+        });
+    };
+
+    var getPrev = function () {
+        ds.getOtherPages(prevPage, (data) => {
+            questions(data.items.$values);
+            nextPage(data.next);
+            prevPage(data.prev);
+        });
+    };
 
     var selectQuestion = (data, submission) => {
         store.dispatch(store.actions.selectQuestion(submission.submissionId));
@@ -13,18 +34,16 @@
     var selectQuestionsByTag = (data, tag) => {
         store.dispatch(store.actions.tagFilter(tag.tag.tagString));
     }
-    var nextPage = () => {
-        ds.moreQuestions((data) => {
-            questions(data.items.$values);
-        });
-    }
     return function () {
         return {
             questions,
             selectQuestion,
             selectQuestionsByTag,
             sessionStorage,
-            nextPage
+            nextPage,
+            prevPage,
+            getNext,
+            getPrev
         };
     };
 });
